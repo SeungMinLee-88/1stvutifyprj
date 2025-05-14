43582f6dbefec70443ab2109b293c9a3be3255e9
lsm0428 <lsmlsm0428@gamil.com>
2025-05-14 16:56:46 +0900

    250514_vue3

--- src/components/ModifyModal.vue
+++ src/components/ModifyModal.vue
@@ -6,6 +6,11 @@ function handleInput(inputVal) {
 console.log("inputVal : " + inputVal)
 
 }
+
+function handleRemove(inputVal) {
+console.log("inputVal : " + inputVal)
+
+}
 </script>
 <template>
   showModal ModifyModal : {{$store.state.showModal}}
@@ -38,6 +43,19 @@ ModifyModal Popup Page
             variant="outlined"
             @update:model-value="handleInput"
       ></v-text-field>
+      <v-list-item
+        v-for="(role, i) in $store.state.userDetail.roleUser"
+        :key="i"
+        :value="role"
+        color="primary"
+      >
+
+
+        <v-list-item-title v-text="role.roleName"></v-list-item-title>
+        <template v-slot:append>
+          <v-icon icon="mdi-close" @click="handleRemove(role.roleId)"></v-icon>
+        </template>
+      </v-list-item>
 
       <v-btn class="mt-2" type="submit" block>Submit</v-btn>
     </v-form>
